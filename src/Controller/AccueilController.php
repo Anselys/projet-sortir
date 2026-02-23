739e6dc576cb73f3920371cba6f192ae2a8fb01c
SebastiaanMoedt <eveningdreamer@proton.me>
2026-02-23 16:36:27 +0100

    query builder de la mort (pour le filtre)

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -7,24 +7,39 @@ use App\Form\TriSortiesType;
 use App\Repository\SiteRepository;
 use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(SortieRepository $sortieRepository, SiteRepository $siteRepository): Response
+    public function index(Request $request, SortieRepository $sortieRepository, SiteRepository $siteRepository): Response
     {
+        $sorties = [];
         $today = new \DateTime();
         $triForm = $this->createForm(TriSortiesType::class);
-        // TODO: get connected user here
-//        $participant = new Participant();
-//        $participant->setSite($siteRepository->findOneByNom('NANTES'));
+        $triForm->handleRequest($request);
+
+        if ($triForm->isSubmitted() && $triForm->isValid()) {
+            $this->$sorties = $sortieRepository->findByTri($triForm);
+
+            $this->addFlash('success', 'Tri activé');
+            return $this->redirectToRoute('', [
+                'sorties' => $sorties,
+                'today' => $today,
+                'tri_form' => $triForm->createView(),
+            ]);
+        }
 //
 //        // par défaut les sorties sont filtrées sur le site de l'utilisateur connecté
 //        $sorties = $sortieRepository->findBySite($participant->getSite());
 
-            $sorties = $sortieRepository->findAll();
+
+        // if result != null:
+        // $this->$sorties  = $sortieRepository->findByTri();
+        // else:
+//        $this->$sorties = $sortieRepository->findAll();
 
 
         return $this->render('accueil/index.html.twig', [
--- src/Repository/SortieRepository.php
+++ src/Repository/SortieRepository.php
@@ -33,13 +33,70 @@ class SortieRepository extends ServiceEntityRepository
             ;
         }
 
-    //    public function findOneBySomeField($value): ?Sortie
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+
+
+
+        public function findByTri($result): array{
+            $task = $result->getData();
+            $qb = $this->createQueryBuilder('s');
+
+            if($task == null){
+                return $this->findAll();
+            }
+
+            if($task['Site'] != null){
+                $qb->andWhere('s.site_organisateur_Id = :siteId')
+                    ->setParameter('siteId', $task['Site']->getId());
+            }
+
+            if($task['recherche'] != null){
+                $qb->andWhere('s.nom LIKE :recherche')
+                    ->setParameter('recherche', '%'.$task['recherche'].'%');
+            }
+
+            if($task['startDate'] != null){
+                $qb->andWhere('s.dateDebut >= :dateDebut')
+                    ->setParameter('dateDebut', $task['dateDebut']);
+            }
+
+            if($task['endDate'] != null){
+                $qb->andWhere('s.dateFin <= :dateFin')
+                    ->setParameter('dateFin', $task['dateFin']);
+            }
+
+            if($task['organisateur'] != 0){
+                // TODO: get connected user here
+//                $qb->andWhere('s.organisateur = :organisateur')
+//                    ->setParameter('organisateur', );
+            }
+
+            if($task['inscrit'] != 0){
+                // TODO: get connected user here
+//                $qb->andWhere('s.inscrit = :inscrit')
+//                    ->setParameter('inscrit', );
+            }
+
+            if($task['non_inscrit'] != 0){
+                // TODO: get connected user here
+//                $qb->andWhere('s.non_inscrit = :inscrit')
+//                    ->setParameter('inscrit', );
+            }
+
+            if($task['passees'] != 0){
+                $qb->andWhere('s.dateDebut < new /DateTime()');
+            }
+
+            return $qb->getQuery()->getResult();
+        }
+
+            /**
+             * si site != null: filter by site (else: all)
+             * & si sortie != '': filter by tout ce qui contient cette recherche (else: all)
+             * & si date début != null : filter by tout ce qui a lieu APRES cette date
+             * & date fin != null : filter by tout ce qui a lieu AVANT cette date
+             * si organisateur coché: filter par seulement organisateur = user (else: all)
+             * si inscrit coché: filter par seulement les inscrits (else: all)
+             * si non_inscrit coché: filter par seulement là ou user n'est PAS inscrit
+             * sorties passées: afficher que les sorties dont la date est passée. (else: all)
+             */
 }
