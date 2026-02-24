51266116b1f2bc4e4932ebe1bc713da78b2bc41f
SebastiaanMoedt <eveningdreamer@proton.me>
2026-02-24 12:21:08 +0100

    tri custom page accueil

--- src/Repository/SortieRepository.php
+++ src/Repository/SortieRepository.php
@@ -2,12 +2,14 @@
 
 namespace App\Repository;
 
+use App\Entity\Etat;
 use App\Entity\Participant;
 use App\Entity\Site;
 use App\Entity\Sortie;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @extends ServiceEntityRepository<Sortie>
@@ -19,83 +21,86 @@ class SortieRepository extends ServiceEntityRepository
         parent::__construct($registry, Sortie::class);
     }
 
-        /**
-         * @return Sortie[] Returns an array of Sortie objects
-         */
-        public function findBySite(Site $site, Participant $participant): array
-        {
-            // ajouter le tri par état plus tard???
-            // TODO: fixme
-            return $this->createQueryBuilder('s')
-                ->andWhere('s.site_organisateur_id = :siteId')
-                ->setParameter('siteId', $site->getId())
-                ->orderBy('s.dateDebut', 'ASC')
-                ->getQuery()
-                ->getResult()
-            ;
+    /**
+     * @return Sortie[] Returns an array of Sortie objects
+     */
+    public function findBySite(Site $site, Etat $etat): array
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.siteOrganisateur = :site')
+            ->andWhere('s.etat = :etat')
+            ->setParameter('etat', $etat)
+            ->setParameter('site', $site)
+            ->orderBy('s.dateDebut', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findAllByEtat(Etat $etat): array
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.etat = :etat')
+            ->setParameter('etat', $etat)
+            ->orderBy('s.dateDebut', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findByTriCustomUtilisateur(FormInterface $triForm, UserInterface $participant): array
+    {
+        $tri = $triForm->getData();
+
+        $qb = $this->createQueryBuilder('s');
+
+        if ($tri == null) {
+            // TODO: return pas tout mais que les etat : ouverte
+            return $this->findAll();
         }
 
-        public function findByTri(FormInterface $result, $participant): array{
-            $tri = $result->getData();
-            $qb = $this->createQueryBuilder('s');
+        if ($tri['Site'] != null) {
+            $qb->andWhere('s.siteOrganisateur = :orgaSite')
+                ->setParameter('orgaSite', $tri['Site']);
+        }
 
-            if($tri == null){
-                return $this->findAll();
-            }
+        if ($tri['recherche'] != null) {
+            $qb->andWhere('s.nom LIKE :recherche')
+                ->setParameter('recherche', '%' . $tri['recherche'] . '%');
+        }
 
-            if($tri['Site'] != null){
-                $qb->andWhere('s.site_organisateur_Id = :siteId')
-                    ->setParameter('siteId', $tri['Site']->getId());
-            }
+        if ($tri['dateDebut'] != null) {
+            $qb->andWhere('s.dateDebut >= :dateDebut')
+                ->setParameter('dateDebut', $tri['dateDebut']);
+        }
 
-            if($tri['recherche'] != null){
-                $qb->andWhere('s.nom LIKE :recherche')
-                    ->setParameter('recherche', '%'.$tri['recherche'].'%');
-            }
+        if ($tri['dateCloture'] != null) {
+            $qb->andWhere('s.dateCloture <= :dateCloture')
+                ->setParameter('dateCloture', $tri['dateCloture']);
+        }
 
-            if($tri['startDate'] != null){
-                $qb->andWhere('s.date_debut >= :dateDebut')
-                    ->setParameter('dateDebut', $tri['dateDebut']);
-            }
+        if ($tri['organisateur'] != 0) {
+            $qb->andWhere('s.organisateur = :organisateur');
+            $qb->setParameter('organisateur', $participant);
+        }
 
-            if($tri['endDate'] != null){
-                $qb->andWhere('s.date_cloture <= :dateCloture')
-                    ->setParameter('dateCloture', $tri['dateFin']);
-            }
-
-            if($tri['organisateur'] != 0){
-                $qb->andWhere('s.organisateur_id = :organisateur')
-                    ->setParameter('organisateur', $participant->getId());
-            }
-
-            if($tri['inscrit'] != 0){
-                // TODO: dépatouiller ça
+        if ($tri['inscrit'] != 0) {
+            // TODO: dépatouiller ça
 //                $participant->getSortiesOrganisees()->findFirst((int)$tri['id'])->setDateFin(new \DateTime($tri['dateFin']));
 //                $qb->andWhere('s.inscrit = :inscrit')
 //                    ->setParameter('inscrit', );
-            }
-
-            if($tri['non_inscrit'] != 0){
-                // TODO: dépatouiller ça
-//                $qb->andWhere('s.non_inscrit = :inscrit')
-//                    ->setParameter('inscrit', );
-            }
-
-            if($tri['passees'] != 0){
-                $qb->andWhere('s.dateDebut < new /DateTime()');
-            }
-
-            return $qb->getQuery()->getResult();
         }
 
-            /**
-             * si site != null: filter by site (else: all)
-             * & si sortie != '': filter by tout ce qui contient cette recherche (else: all)
-             * & si date début != null : filter by tout ce qui a lieu APRES cette date
-             * & date fin != null : filter by tout ce qui a lieu AVANT cette date
-             * si organisateur coché: filter par seulement organisateur = user (else: all)
-             * si inscrit coché: filter par seulement les inscrits (else: all)
-             * si non_inscrit coché: filter par seulement là ou user n'est PAS inscrit
-             * sorties passées: afficher que les sorties dont la date est passée. (else: all)
-             */
+        if ($tri['non_inscrit'] != 0) {
+            // TODO: dépatouiller ça
+//                $qb->andWhere('s.non_inscrit = :inscrit')
+//                    ->setParameter('inscrit', );
+        }
+
+        if ($tri['passees'] != 0) {
+            // TODO: dépatouiller ça
+            // $qb->andWhere('s.dateDebut < new /DateTime()');
+        }
+
+        return $qb->getQuery()->getResult();
+    }
+
 }
