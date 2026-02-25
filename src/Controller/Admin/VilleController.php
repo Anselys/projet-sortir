8e19a94a6b2d0a88a6005688a02fab825aec31b2
SebastiaanMoedt <eveningdreamer@proton.me>
2026-02-25 10:44:39 +0100

    petite opti accueilcontroller

--- src/Controller/Admin/VilleController.php
+++ src/Controller/Admin/VilleController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Ville;
+use App\Form\VilleType;
+use App\Repository\VilleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -12,10 +17,31 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 final class VilleController extends AbstractController
 {
     #[Route('/ville', name: '_ville')]
-    public function index(): Response
+    public function index(Request $request, VilleRepository $villeRepository, EntityManagerInterface $em): Response
     {
+        $villesForm = $this->createForm(VilleType::class);
+        $villesForm->handleRequest($request);
+        // si un filtre de tri est soumis, sorties est rempli via le tri
+        if ($villesForm->isSubmitted() && $villesForm->isValid())
+        {
+            $ville = $villesForm->getData();
+            $villeExists = $villeRepository->findOne($ville);
+            if(!$villeExists)
+            {
+                $em->persist($ville);
+                $em->flush();
+                $this->addFlash('success', 'La ville a été ajoutée avec succès');
+            }
+            else
+            {
+                $this->addFlash('warning', 'Cette ville existe déjà');
+            }
+        }
+        $villes = $villeRepository->findAll();
+
         return $this->render('admin/ville.html.twig', [
-            'controller_name' => 'Admin/VilleController',
+            'villes' => $villes,
+            'villes_form' => $villesForm->createView(),
         ]);
     }
 }
