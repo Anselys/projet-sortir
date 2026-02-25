2e362be89c23a5a1488ca211ba3b83b4bde5c5ec
SebastiaanMoedt <eveningdreamer@proton.me>
2026-02-25 15:48:41 +0100

    admin/site WORKING!

--- src/Controller/Admin/SiteController.php
+++ src/Controller/Admin/SiteController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Site;
+use App\Form\SearchType;
+use App\Form\SiteType;
+use App\Repository\SiteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -12,10 +18,84 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 final class SiteController extends AbstractController
 {
     #[Route('/site', name: '_site')]
-    public function index(): Response
+    public function index(Request $request, SiteRepository $siteRepository, EntityManagerInterface $em): Response
     {
+        $siteForm = $this->createForm(SiteType::class);
+        $siteForm->handleRequest($request);
+
+        // si un site a été ajouté: passer par ici
+        if ($siteForm->isSubmitted() && $siteForm->isValid()) {
+            $site = $siteForm->getData();
+            $siteExists = $siteRepository->findOneByNom($site->getNom());
+            if (!$siteExists) {
+                $em->persist($site);
+                $em->flush();
+                $this->addFlash('success', 'Le site a été ajoutée avec succès');
+            } else {
+                $this->addFlash('warning', 'Ce site existe déjà');
+            }
+        }
+        $searchForm = $this->createForm(SearchType::class);
+        $searchForm->handleRequest($request);
+        // si une recherche est effectuée, passer par ici
+        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
+            $search = $searchForm->getData();
+            if ($search['nom']) {
+                $sites = $siteRepository->searchByName($search['nom']);
+            } else {
+                $sites = $siteRepository->findAll();
+            }
+        } // si ni recherche ni ajout, afficher tous les sites.
+        else {
+            $sites = $siteRepository->findAll();
+        }
+
         return $this->render('admin/site.html.twig', [
-            'controller_name' => 'Admin/SiteController',
+            'sites' => $sites,
+            'search_form' => $searchForm->createView(),
+            'site_form' => $siteForm->createView(),
         ]);
     }
+
+
+    #[Route('/site/update/{id}', name: '_site_update', requirements: ['id' => '\d+'])]
+    public function update(Site $site, EntityManagerInterface $em, Request $request): Response
+    {
+        $siteUpdateForm = $this->createForm(SiteType::class);
+        $siteUpdateForm->handleRequest($request);
+        if ($siteUpdateForm->isSubmitted() && $siteUpdateForm->isValid()) {
+            // récupèrer les nouvelles données ville
+            $site = $siteUpdateForm->getData();
+            $siteFound = $em->getRepository(Site::class)->findOneByNom($site->getNom());
+            // checker si une ville sous ce combo nom/cpo existe déjà.
+            if (!$siteFound) {
+                $em->flush();
+                $this->addFlash('success', 'Le site a été modifié avec succès');
+                return $this->redirectToRoute('app_admin_site');
+            }
+            $this->addFlash('danger', "Il y a déjà un site de ce nom dans la base de données.");
+            return $this->redirectToRoute('app_admin_site');
+        }
+        return $this->render('admin/site-edit.html.twig', [
+            'site_update_form' => $siteUpdateForm->createView(),
+            'site' => $site,
+        ]);
+    }
+
+
+    #[Route('/site/delete/{id}', name: '_site_delete', requirements: ['id' => '\d+'])]
+    public function delete(Site $site, EntityManagerInterface $em, Request $request): Response
+    {
+        $token = $request->query->get('token');
+        if ($this->isCsrfTokenValid('site_delete' . $site->getId(), $token)) {
+            $em->remove($site);
+            $em->flush();
+            $this->addFlash('success', 'Le site a été supprimé');
+            return $this->redirectToRoute('app_admin_site');
+        }
+
+        $this->addFlash('danger', 'Impossible de supprimer ce site.');
+        return $this->redirectToRoute('app_admin_site', ['id' => $site->getId()]);
+    }
+
 }
