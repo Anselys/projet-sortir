d33c9f3cbe861909d1e419cfc4973da116d26b68
SebastiaanMoedt <eveningdreamer@proton.me>
2026-03-02 14:52:23 +0100

    annuler sortie en cours

---
+++ src/Form/AnnulationType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnulationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('motif', TextType::class, [
+                'label' => 'Motif de l\'annulation: ',
+                'required' => true,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Annuler la sortie',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
