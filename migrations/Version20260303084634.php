7a38d876d1f07f4380453a2c0dd704c9e0bd8a64
Mickael-Mesloub <mickael.mesloub@gmail.com>
2026-03-03 09:59:04 +0100

    Ajout reset password fonctionnel

--- migrations/Version20260303084634.php
+++ migrations/Version20260303084634.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260225110854 extends AbstractMigration
+final class Version20260303084634 extends AbstractMigration
 {
     public function getDescription(): string
     {
